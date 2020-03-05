f0d37411fe715c8f0ec6e8bbe8ea30e49f980525
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-05 14:05:22 +0100

    Added grid search cross-validation

--- amr_maldi_ml/curves_per_species_and_antibiotic.py
+++ amr_maldi_ml/curves_per_species_and_antibiotic.py
@@ -5,6 +5,8 @@ import dotenv
 import json
 import os
 
+import numpy as np
+
 from maldi_learn.driams import DRIAMSDatasetExplorer
 from maldi_learn.driams import DRIAMSLabelEncoder
 
@@ -18,6 +20,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import ParameterGrid
+from sklearn.model_selection import GridSearchCV
 
 dotenv.load_dotenv()
 DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
@@ -38,8 +41,9 @@ if __name__ == '__main__':
     years = ['2015', '2017']
     _seeds = [123, 321]
 
-    #  create param grid for experiments
-    grid = ParameterGrid([
+    # Create input grid for the subsequent experiments. Not all
+    # combinations are useful, hence we specify them in a list.
+    input_grid = ParameterGrid([
         {
             'species': ['Escherichia coli'],
             'antibiotics': ['Ciprofloxacin'],
@@ -59,7 +63,7 @@ if __name__ == '__main__':
     # Set of default parameters; should be made adjustable for running
     # the comparison at larger scales.
 
-    for combination in grid:
+    for combination in input_grid:
         print(combination)
 
         driams_dataset = load_driams_dataset(
@@ -90,14 +94,40 @@ if __name__ == '__main__':
         X_train, y_train = X[train_index], y[train_index]
         X_test, y_test = X[test_index], y[test_index]
 
-        # Fit the classifier and start calculating some summary metrics
-        lr = LogisticRegression()
-        lr.fit(X_train, y_train)
-        y_pred = lr.predict(X_test)
-        y_score = lr.predict_proba(X_test)
+        param_grid = [
+            {
+                'lr__C': 10.0 ** np.arange(-3, 4),  # 10^{-3}..10^{3}
+                'lr__penalty': ['l1', 'l2'],
+            },
+            {
+                'lr__penalty': ['none'],
+            }
+        ]
+
+        n_folds = 5
+
+        # Fit the classifier and start calculating some summary metrics.
+        # All of this is wrapped in cross-validation based on the grid
+        # defined above.
+        lr = LogisticRegression(solver='saga')
+        grid_search = GridSearchCV(
+                        lr,
+                        param_grid=param_grid,
+                        cv=n_folds,
+                        scoring='roc_auc'
+        )
+
+        grid_search.fit(X_train, y_train)
+        y_pred = grid_search.predict(X_test)
+        y_score = grid_search.predict_proba(X_test)
 
         accuracy = accuracy_score(y_pred, y_test)
-        auprc = average_precision_score(y_test, y_score[:, 1], average='weighted')
+
+        auprc = average_precision_score(
+                    y_test, y_score[:, 1],
+                    average='weighted'
+        )
+
         auroc = roc_auc_score(y_test, y_score[:, 1], average='weighted')
 
         # Prepare the output dictionary containing all information to
@@ -105,6 +135,7 @@ if __name__ == '__main__':
 
         output = {
             'site': site,
+            'seed': combination['seed'],
             'antibiotics': combination['antibiotics'],
             'species': combination['species'],
             'years': years,
@@ -121,4 +152,5 @@ if __name__ == '__main__':
             indent=4
         ))
 
-    # TODO: generate filename for input arguments
+        # TODO: generate filename for input arguments
+        # TODO: store output
