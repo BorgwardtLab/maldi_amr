5e0109ed267ee7cc8c3c82a0ca16842c29b67c39
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-05 14:25:33 +0100

    Utility function for creating an output filename

---
+++ amr_maldi_ml/utilities.py
@@ -0,0 +1,40 @@
+"""Utility functions."""
+
+
+import os
+
+
+def generate_output_filename(root, data):
+    """Generate output filename for dictionary.
+
+    Given a root folder for storing output files, creates a filename
+    based on a data dictionary.
+
+    Parameters
+    ----------
+    root : str
+        Output directory for storing the data
+
+    data : dict
+        Data dictionary; containing a few required keys, such as
+        `antibiotic`, and many optional ones.
+
+    Returns
+    -------
+    Filename for storing the output. This filename is not guaranteed to
+    be unique and it is the client's responsibility to check whether it
+    exists.
+    """
+    # Default to using *all* species if the key is not present in the
+    # data dictionary.
+    species = data.get('species', 'all')
+
+    antibiotic = data['antibiotic']
+    seed = data['seed']
+    site = data['site']
+
+    filename = f'Site_{site}_' \
+               f'Species_{species}_Antibiotic_{antibiotic}_Seed_{seed}.json'
+    filename = os.path.join(root, filename)
+
+    return filename
