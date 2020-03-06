aeaf4f779be96d5227ecbd42cd0232e87ac2efb6
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-03-06 16:09:58 +0100

    add color map

--- amr_maldi_ml/utilities.py
+++ amr_maldi_ml/utilities.py
@@ -38,3 +38,55 @@ def generate_output_filename(root, data):
     filename = os.path.join(root, filename)
 
     return filename
+
+
+
+# define color maps
+col_list = [u'orangered', u'purple', u'royalblue', u'olivedrab', u'orange', u'violet',  u'red', u'turquoise', u'chartreuse', u'black', u'saddlebrown', u'salmon', u'mediumvioletred', u'seagreen', u'skyblue', u'slateblue', u'darkgrey', u'springgreen', u'teal', u'tomato', u'peru', u'yellowgreen', u'aqua', u'aquamarine', u'blue', u'blueviolet', u'brown', u'burlywood', u'cadetblue', u'chocolate', u'coral', u'cornflowerblue', u'crimson', u'darkblue', u'darkcyan', u'darkgoldenrod', u'darkgreen', u'darkgrey', u'darkmagenta', u'hotpink', u'darkolivegreen', u'yellow']
+
+
+#TODO adjust antibiotics to general naming
+maldi_col_map = {
+    'Ceftriaxon': col_list[0],
+    'Oxacillin': col_list[1],
+    'Amoxicillin-Clavulanic acid': col_list[2],
+    'Meropenem': col_list[3],
+    'Piperacillin...Tazobactam': col_list[4],
+    'Ciprofloxacin': col_list[5],
+    'Colistin': col_list[6],
+    'Fluconazol': col_list[7],
+    'Fusidinsaeure': col_list[9],
+    'Cefepim': col_list[8],
+    'Penicillin': col_list[10],
+    'Imipenem': col_list[11],
+    'Gentamicin': col_list[12],
+    'Tetracyclin': col_list[13],
+    'Vancomycin': col_list[14],
+    'Clindamycin': col_list[15],
+    'Nitrofurantoin': col_list[16],
+    'Tigecyclin': col_list[17],
+    'Tobramycin': col_list[18],
+    'Amikacin': col_list[19],
+    'Amoxicillin': col_list[20],
+    'Ampicillin...Amoxicillin': col_list[21],
+    'Anidulafungin': col_list[22],
+    'Aztreonam': col_list[23],
+    'Caspofungin': col_list[24],
+    'Cefazolin': col_list[25],
+    'Cefpodoxim': col_list[26],
+    'Ceftazidim': col_list[27],
+    'Cefuroxim': col_list[28],
+    'Cotrimoxazol': col_list[29],
+    'Daptomycin': col_list[30],
+    'Ertapenem': col_list[31],
+    'Erythromycin': col_list[32],
+    'Fosfomycin.Trometamol': col_list[33],
+    'Itraconazol': col_list[34],
+    'Levofloxacin': col_list[35],
+    'Micafungin': col_list[36],
+    'Norfloxacin': col_list[37],
+    'Rifampicin': col_list[38],
+    'Teicoplanin': col_list[39],
+    'Voriconazol': col_list[40],
+    'X5.Fluorocytosin': col_list[41]
+    }
