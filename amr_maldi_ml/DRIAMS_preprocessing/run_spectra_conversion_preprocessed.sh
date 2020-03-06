e57de09e4e7debcb50e6c061fb11701d9d843977
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-03-06 10:46:50 +0100

    run fixed R preprocesseding, include log files

--- amr_maldi_ml/DRIAMS_preprocessing/run_spectra_conversion_preprocessed.sh
+++ amr_maldi_ml/DRIAMS_preprocessing/run_spectra_conversion_preprocessed.sh
@@ -1,6 +1,6 @@
 
-#Rscript DRIAMS-A_2018_preprocessed.r
-#Rscript DRIAMS-A_2017_preprocessed.r
+Rscript DRIAMS-A_2018_preprocessed.r
+Rscript DRIAMS-A_2017_preprocessed.r
 Rscript DRIAMS-A_2016_preprocessed.r
 Rscript DRIAMS-A_2015_preprocessed.r
 
