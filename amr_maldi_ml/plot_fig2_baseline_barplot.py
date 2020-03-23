84d577647e60dfe6d576977b1485f5d5fa50ec45
Caroline Weis <carolineweis@gmx.de>
2020-03-23 17:28:03 +0100

    syntax fixes

--- amr_maldi_ml/plot_fig2_baseline_barplot.py
+++ amr_maldi_ml/plot_fig2_baseline_barplot.py
@@ -145,11 +145,10 @@ def plot_figure2(args):
     # p-values
     pval_string = ['*' if pv < 0.05 else '' for pv in
                    values['pvals'].values]
-    print(list(zip(pval_string, values['pvals'].values)))
     for i, yval in enumerate(values['auroc_all'].values):
         ax.annotate(
-                    '{:.1e}'.format(values['pvals'].iloc[i]),
-                    # pval_string[i],
+                    # '{:.1e}'.format(values['pvals'].iloc[i]),
+                    pval_string[i],
                     xy=(i, yval),
                     xytext=(i-0.15,  # -0.2 for text, -0.15 for stars
                             yval+values['auroc_std_all'].iloc[i]+0.01),
