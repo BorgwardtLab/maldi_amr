f618c54bec81844df75984d6ce497c331014fa77
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-04-14 18:11:57 +0200

    update with DRIAMS-C and -D

--- amr_maldi_ml/DRIAMS_preprocessing/id_files/run_id_cleaning.sh
+++ amr_maldi_ml/DRIAMS_preprocessing/id_files/run_id_cleaning.sh
@@ -13,5 +13,16 @@ python DRIAMS-A_clean_id_file.py /links/groups/borgwardt/Projects/maldi_tof_diag
 python remove_overlap_17_18.py
 
 # DRIAMS-B
-# python DRIAMS-B_clean_id_file.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/KSBL/KSBL_res_report.csv \
-#                         /links/groups/borgwardt/Data/DRIAMS/DRIAMS-B/id/2018/2018_clean.csv 
+python DRIAMS-B_clean_id_file.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/KSBL/KSBL_res_report.csv \
+                        /links/groups/borgwardt/Data/DRIAMS/DRIAMS-B/id/2018/2018_clean.csv 
+
+# DRIAMS-C
+jupyter nbconvert --to script DRIAMS-C_I_divide_spectra_into_packages_for_Bruker_DB_analysis.ipynb 
+python DRIAMS-C_I_divide_spectra_into_packages_for_Bruker_DB_analysis.ipy
+jupyter nbconvert --to script DRIAMS-C_II_match_with_Bruker_DB_output_files.ipynb
+python DRIAMS-C_II_match_with_Bruker_DB_output_files.ipy
+
+# DRIAMS-D
+jupyter nbconvert --to script DRIAMS-D_clean_id_file.ipynb
+python DRIAMS-D_clean_id_file.ipy
+
