57406a4129f4e4a3756f6bdb2ec7d187b106659c
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-04-15 08:49:21 +0200

    Simplified feature importance calculation
    The script is now ready to run; old code from previous versions has
    been removed and the new convenience function from `models` is used
    now.

--- amr_maldi_ml/calculate_feature_importance_values.py
+++ amr_maldi_ml/calculate_feature_importance_values.py
@@ -16,12 +16,9 @@ from maldi_learn.driams import load_driams_dataset
 
 from maldi_learn.utilities import stratify_by_species_and_label
 
-from models import get_pipeline_and_parameters
+from models import load_pipeline
 
 from utilities import generate_output_filename
-
-from sklearn.model_selection import ParameterGrid
-from sklearn.preprocessing import StandardScaler
 
 dotenv.load_dotenv()
 DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
@@ -31,99 +28,6 @@ DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
 # *all* available years.
 site = 'DRIAMS-A'
 years = ['2015', '2016', '2017', '2018']
-
-
-def _run_experiment(
-    root,
-    fingerprints,
-    species,
-    antibiotic,
-    seed,
-    output_path,
-    force,
-    model,
-    n_jobs=-1
-):
-    """Run a single experiment for a given species--antibiotic combination."""
-    driams_dataset = load_driams_dataset(
-            root,
-            site,
-            years,
-            species=species,
-            antibiotics=antibiotic,  # Only a single one for this run
-            encoder=DRIAMSLabelEncoder(),
-            handle_missing_resistance_measurements='remove_if_all_missing',
-            spectra_type='binned_6000',
-    )
-
-    logging.info(f'Loaded data set for {species} and {antibiotic}')
-
-    # Create feature matrix from the binned spectra. We only need to
-    # consider the second column of each spectrum for this.
-    X = np.asarray([spectrum.intensities for spectrum in driams_dataset.X])
-
-    logging.info('Finished vectorisation')
-
-    # Stratified train--test split
-    train_index, test_index = stratify_by_species_and_label(
-        driams_dataset.y,
-        antibiotic=antibiotic,
-        random_state=seed,
-    )
-
-    logging.info('Finished stratification')
-
-    # Create labels
-    y = driams_dataset.to_numpy(antibiotic)
-
-    X_train, y_train = X[train_index], y[train_index]
-    X_test, y_test = X[test_index], y[test_index]
-
-    # Prepare the output dictionary containing all information to
-    # reproduce the experiment.
-    output = {
-        'site': site,
-        'seed': seed,
-        'model': model,
-        'antibiotic': antibiotic,
-        'species': species,
-        'years': years,
-    }
-
-    output_filename = generate_output_filename(
-        output_path,
-        output
-    )
-
-    # Add fingerprint information about the metadata files to make sure
-    # that the experiment is reproducible.
-    output['metadata_versions'] = fingerprints
-
-    # Only write if we either are running in `force` mode, or the
-    # file does not yet exist.
-    if not os.path.exists(output_filename) or force:
-
-        n_folds = 5
-
-        results = run_experiment(
-            X_train, y_train,
-            X_test, y_test,
-            model,
-            n_folds,
-            verbose=True,
-            random_state=seed,
-        )
-
-        output.update(results)
-
-        logging.info(f'Saving {os.path.basename(output_filename)}')
-
-        with open(output_filename, 'w') as f:
-            json.dump(output, f, indent=4)
-    else:
-        logging.warning(
-            f'Skipping {output_filename} because it already exists.'
-        )
 
 
 if __name__ == '__main__':
@@ -161,14 +65,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # Load configuration and output (which we will subsequently ignore,
-    # though, because we do not need it).
-    with open(args.INPUT) as f:
-        data = json.load(f)
-
     # Create the output directory for storing all results of the
     # individual combinations.
     os.makedirs(args.output, exist_ok=True)
+
+    pipeline, data = load_pipeline(args.INPUT)
 
     antibiotic = data['antibiotic']
     site = data['site']
@@ -176,22 +77,11 @@ if __name__ == '__main__':
     seed = data['seed']
     species = data['species']
     best_params = data['best_params']
-
-    # This accommodates older versions of the code that do not save
-    # their models to the file.
-    model = data.get('model', 'lr')
+    model = data['model']
 
     logging.info(f'Site: {site}')
     logging.info(f'Years: {years}')
     logging.info(f'Seed: {seed}')
-
-    pipeline, _ = get_pipeline_and_parameters(model, random_state=seed)
-    pipeline.set_params(**best_params)
-
-    # Scaling needs some manual adjustments because we just store
-    # whether a scaler was used or not.
-    if pipeline['scaler'] == 'StandardScaler':
-        pipeline.set_params(scaler=StandardScaler())
 
     explorer = DRIAMSDatasetExplorer(DRIAMS_ROOT)
     metadata_fingerprints = explorer.metadata_fingerprints(site)
@@ -205,7 +95,6 @@ if __name__ == '__main__':
             encoder=DRIAMSLabelEncoder(),
             handle_missing_resistance_measurements='remove_if_all_missing',
             spectra_type='binned_6000',
-            nrows=1000,
     )
 
     logging.info(f'Loaded data set for {species} and {antibiotic}')
