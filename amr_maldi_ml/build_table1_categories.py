cadca28514fbf3e71fadc4ab818b243ad71bef9e
Caroline Weis <carolineweis@gmx.de>
2020-04-20 14:28:28 +0200

    sort by number of samples and add total count row

--- amr_maldi_ml/build_table1_categories.py
+++ amr_maldi_ml/build_table1_categories.py
@@ -49,7 +49,7 @@ def create_table(args):
 
     
     # create new table with categorized antibiotics 
-    table_categorized = pd.DataFrame(columns=['category',
+    table_categorized = pd.DataFrame(columns=['antimicrobial class',
                                               'antibiotic',
                                               'site',
                                               'number of samples',
@@ -59,7 +59,7 @@ def create_table(args):
     for index, row in table.iterrows():
         table_categorized = table_categorized.append(
             pd.DataFrame({
-                'category' : [ab_cat_map[row['antibiotic']]],
+                'antimicrobial class' : [ab_cat_map[row['antibiotic']]],
                 'antibiotic' : [row['antibiotic']],
                 'site': [row['site']],
                 'number of samples': [row['number of samples']],
@@ -71,21 +71,41 @@ def create_table(args):
     # create column with positive numbers
     table_categorized['number positive samples'] = table_categorized['number of samples'] * table_categorized['percentage positive']
     
-    # group rows by category
-    table_categorized = table_categorized.groupby('category').agg({'antibiotic': lambda x: ', '.join(x), 
+    # add 'total' row 
+    table_categorized = table_categorized.append(
+        pd.DataFrame({
+            'antimicrobial class' : ['total'],
+            'antibiotic' : [''],
+            'site': [args.site],
+            'number of samples': [table_categorized['number of samples'].sum()],
+            'number positive samples': [table_categorized['number positive samples'].sum()],
+        }),
+        ignore_index=True
+        ) 
+
+    # group rows by antimicrobial class
+    table_categorized = table_categorized.groupby('antimicrobial class').agg({'antibiotic': lambda x: ', '.join(x), 
                                                                    'number of samples': 'sum',
                                                                    'number positive samples': 'sum'})
-    # create column with positive class percentage
+    # recreate column with positive class percentage
     table_categorized['percentage positive'] = table_categorized['number positive samples'] / table_categorized['number of samples']    
     table_categorized = table_categorized.drop(columns=['number positive samples'])
     table_categorized['percentage positive'] = table_categorized['percentage positive'].round(3)
 
+    # sort rows by sample size, 'total' row last
+    table_categorized = table_categorized.sort_values(by=['number of samples'],
+                                                      ascending=False) 
+    # TODO put 'total' row last
+    
+    # sort columns in order in which they appear in the paper
+    table_categorized = table_categorized[['number of samples',
+                                           'percentage positive',
+                                           'antibiotic']]
+
     # rename column headers to the final header naming
     table_categorized = table_categorized.rename(columns={'antibiotic': 'antimicrobials', 
-                                                          'category': 'antimicrobial class',
                                                           'number of samples': 'number of samples in class',
                                                           'percentage positive': 'avg. positive sample ratio'})
-
 
 
     if args.save == True:
