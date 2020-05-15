bd1a663d9685d8b165d1e1c67cdd5d9eb0413cbb
Caroline Weis <carolineweis@gmx.de>
2020-05-15 18:41:10 +0200

    rejection tables

--- amr_maldi_ml/create_table.sh
+++ amr_maldi_ml/create_table.sh
@@ -1,19 +1,27 @@
 
 
-# ------------
-# Table 1: Create summary table for antibiotic prevalences 
-#          categorizes by antibiotic class 
-# ------------
+## ------------
+## Table 1: Create summary table for antibiotic prevalences 
+##          categorizes by antibiotic class 
+## ------------
+#
+#python build_table1_categories.py --site "DRIAMS-A" \
+#      --save True \
+#      --outfile "plots/tables/DRIAMS-A_Table1.csv"
+#
+## ------------
+## Table 1 supplement: Create full summary table for antibiotic prevalences 
+## ------------
+#
+#python build_table1_suppl.py --site "DRIAMS-A" \
+#      --save True \
+#      --remove_empty_antibiotics True \
+#      --outfile "plots/tables/DRIAMS-A_Table1_suppl.csv"
+#
 
-python build_table1_categories.py --site "DRIAMS-A" \
-      --save True \
-      --outfile "plots/tables/DRIAMS-A_Table1.csv"
-
 # ------------
-# Table 1 supplement: Create full summary table for antibiotic prevalences 
+# Table 2: Rejection ratio tables
 # ------------
+python build_table2_rejection.py ../results/calibrated_classifiers \
+--outdir plots/tables/rejection 
 
-python build_table1_suppl.py --site "DRIAMS-A" \
-      --save True \
-      --remove_empty_antibiotics True \
-      --outfile "plots/tables/DRIAMS-A_Table1_suppl.csv"
