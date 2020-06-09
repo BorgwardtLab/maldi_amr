68a1c8f6648f675cd713deb82f01b68dc40ef534
Caroline Weis <carolineweis@gmx.de>
2020-06-09 16:24:41 +0200

    update

--- amr_maldi_ml/create_table.sh
+++ amr_maldi_ml/create_table.sh
@@ -1,23 +1,23 @@
 
 
-## ------------
-## Table 1: Create summary table for antibiotic prevalences 
-##          categorizes by antibiotic class 
-## ------------
-#
-#python build_table1_categories.py --site "DRIAMS-A" \
-#      --save True \
-#      --outfile "plots/tables/DRIAMS-A_Table1.csv"
-#
-## ------------
-## Table 1 supplement: Create full summary table for antibiotic prevalences 
-## ------------
-#
-#python build_table1_suppl.py --site "DRIAMS-A" \
-#      --save True \
-#      --remove_empty_antibiotics True \
-#      --outfile "plots/tables/DRIAMS-A_Table1_suppl.csv"
-#
+# ------------
+# Table 1: Create summary table for antibiotic prevalences 
+#          categorizes by antibiotic class 
+# ------------
+
+python build_table1_categories.py --site "DRIAMS-A" \
+      --save True \
+      --outfile "plots/tables/DRIAMS-A_Table1.csv"
+
+# ------------
+# Table 1 supplement: Create full summary table for antibiotic prevalences 
+# ------------
+
+python build_table1_suppl.py --site "DRIAMS-A" \
+      --save True \
+      --remove_empty_antibiotics True \
+      --outfile "plots/tables/DRIAMS-A_Table1_suppl.csv"
+
 
 # ------------
 # Table 2: Rejection ratio tables
@@ -25,8 +25,8 @@
 #python build_table2_rejection.py ../results/calibrated_classifiers \
 #--outdir plots/tables/rejection 
 
-#python build_table2_assymetric_rejection.py ../results/calibrated_classifiers \
-#--outdir plots/tables/rejection 
+python build_table2_assymetric_rejection.py ../results/calibrated_classifiers \
+--outdir plots/tables/rejection 
 
 # ------------
 # Table 3: Replication table
