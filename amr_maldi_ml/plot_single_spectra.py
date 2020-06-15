0722e41e74b858cf80494271ec4edc6a566bc079
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-06-15 16:54:28 +0200

    plot single example spectrum

--- amr_maldi_ml/plot_single_spectra.py
+++ amr_maldi_ml/plot_single_spectra.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from maldi_learn.driams import load_driams_dataset
 from maldi_learn.driams import DRIAMSDatasetExplorer
@@ -16,18 +17,16 @@ from maldi_learn.driams import DRIAMSLabelEncoder
 load_dotenv()
 DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
 
-pair = [
-    ('Escherichia coli', 'Ciprofloxacin'),
-			  ]
+pair = ('Escherichia coli', 'Ciprofloxacin')
 
-def load_single_raw_spectrum(pair, year, site):
+def load_single_raw_spectrum(pair_, year, site):
 
     driams = load_driams_dataset(
     DRIAMS_ROOT,
     site,
     year,
-    pair[0],
-    pair[1],
+    pair_[0],
+    pair_[1],
     encoder=DRIAMSLabelEncoder(),
     handle_missing_resistance_measurements='remove_if_any_missing',
     spectra_type='raw',
@@ -41,5 +40,14 @@ def load_single_raw_spectrum(pair, year, site):
 if __name__=='__main__':
     
     raw_spectrum = load_single_raw_spectrum(pair, '2018', 'DRIAMS-A')
-    print(raw_spectrum)
+    
+    sns.set(style='whitegrid')
+    fig, ax = plt.subplots(figsize=(10,7))
+    
+    ax.plot(raw_spectrum[:,0], raw_spectrum[:,1])
+    ax.set_xlim(2000,15000)
+    ax.set_ylabel('intensity')
+    ax.set_xlabel('m/z')    
+
+    plt.savefig('./plots/example_spectra.png')
 
