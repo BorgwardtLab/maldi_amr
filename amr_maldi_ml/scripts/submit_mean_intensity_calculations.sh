c6b56d5fb7dadacd19e381ffec7873eeec947717
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-07-08 12:33:35 +0200

    New job submission script for mean intensity calculations

---
+++ amr_maldi_ml/scripts/submit_mean_intensity_calculations.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# The purpose of this script is to submit mean intensity calculation
+# jobs to an LSF system in order to speed up job processing. This is
+# the sibling script to the feature importance value calculations.
+
+# Main command to execute for all combinations created in the script
+# below. The space at the end of the string is important.
+MAIN="poetry run python ../mean_intensities.py "
+
+# Try to be smart: if `bsub` does *not* exist on the system, we just
+# pretend that it is an empty command.
+if [ -x "$(command -v bsub)" ]; then
+  BSUB='bsub -W 3:59 -o "mean_intensities_%J.out" -R "rusage[mem=32000]"'
+fi
+
+# Evaluates its first argument either by submitting a job, or by
+# executing the command without parallel processing.
+run() {
+  if [ -z "$BSUB" ]; then
+    eval "$1";
+  else
+    eval "${BSUB} $1";
+  fi
+}
+
+# S. aureus jobs
+for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*aureus*Oxacillin*"); do
+  run "${MAIN} ${FILE}"
+done
+
+# K. pneumoniae jobs
+for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*pneu*Meropenem*"); do
+  run "${MAIN} ${FILE}"
+done
+
+# E. coli jobs
+for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*coli*Ceftriaxone*"); do
+  run "${MAIN} ${FILE}"
+done
