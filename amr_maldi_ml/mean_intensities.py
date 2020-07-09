Fixed normalisation for mean spectrum calculation

amr_maldi_ml/mean_intensities.py
@@ -143,10 +143,10 @@ if __name__ == '__main__':
         for l in np.unique(y_train):
             spectra = X_train[y_train == l]
             mean_intensities[str(l)] = np.mean(spectra, axis=0)
-
+            
             # We do *not* yet convert the resulting array because it is
             # to keep `np.array` around for sums etc.
-            if l in all_mean_intensities:
+            if str(l) in all_mean_intensities:
                 all_mean_intensities[str(l)] += mean_intensities[str(l)]
             else:
                 all_mean_intensities[str(l)] = mean_intensities[str(l)]
@@ -201,6 +201,8 @@ if __name__ == '__main__':
 
         for l in all_mean_intensities:
             mean_intensities[l] = all_mean_intensities[l] / len(args.INPUT)
+            print(len(mean_intensities[l]))
+            print(sum(mean_intensities[l]))
             mean_intensities[l] = mean_intensities[l].ravel().tolist()
 
         sites = list(set(all_sites))