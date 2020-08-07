e71c6c258dbcaf82cf2afec14044a352cc4c5619
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-08-07 09:43:00 +0200

    Proper output for sample size analysis

--- amr_maldi_ml/analyse_sample_size_versus_auc.py
+++ amr_maldi_ml/analyse_sample_size_versus_auc.py
@@ -14,6 +14,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('DIRECTORY')
 
+    parser.add_argument(
+        '-m', '--metric',
+        type=str,
+        help='Performance metric to monitor',
+        default='auroc'
+    )
+
+    parser.add_argument(
+        '-t', '--threshold',
+        type=float,
+        help='Threshold for the given performance metric',
+        default='0.90',
+    )
+
     args = parser.parse_args()
 
     filenames = sorted(glob.glob(os.path.join(args.DIRECTORY, '*.json')))
@@ -31,7 +45,21 @@ if __name__ == '__main__':
                 'accuracy': data['accuracy'],
                 'auprc': data['auprc'],
                 'auroc': data['auroc'],
+                'species': data['species'],
+                'antibiotic': data['antibiotic']
             })
 
     df = pd.DataFrame(rows)
-    print(df.to_csv(index=False))
+    df = df.sort_values(['n_samples', 'species', 'antibiotic'])
+
+    # Keep only the *first* row, i.e. the row with the smallest number
+    # of samples.
+    df = df[df[args.metric] >= args.threshold].groupby(
+        ['species', 'antibiotic']
+    ).first()
+
+    print(f'Metric: {args.metric}')
+    print(f'Threshold: {args.threshold}')
+
+    n_samples_mean = df['n_samples'].mean()
+    print(f'Require on average {n_samples_mean:.0f} samples')
