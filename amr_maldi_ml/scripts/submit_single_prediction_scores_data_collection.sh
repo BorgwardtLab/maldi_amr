07ada745c3b817efe7cf13b0a31899890bb9d6ac
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-10-30 19:58:38 +0100

    single_prediction_scores_data_collection

---
+++ amr_maldi_ml/scripts/submit_single_prediction_scores_data_collection.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# The purpose of this script is to submit feature importance calculation
+# jobs to an LSF system in order to speed up job processing.
+
+# Main command to execute for all combinations created in the script
+# below. The space at the end of the string is important.
+MAIN="poetry run python ../single_prediction_scores.py "
+
+# Try to be smart: if `bsub` does *not* exist on the system, we just
+# pretend that it is an empty command.
+if [ -x "$(command -v bsub)" ]; then
+  BSUB='bsub -W 3:59 -o "feature_importance_values_%J.out" -R "rusage[mem=32000]"'
+fi
+
+# Evaluates its first argument either by submitting a job, or by
+# executing the command without parallel processing.
+run() {
+  if [ -z "$BSUB" ]; then
+    eval "$1";
+  else
+    eval "${BSUB} $1";
+  fi
+}
+
+
+# S. aureus jobs
+for code in $(cat /links/groups/borgwardt/Data/ms_diagnostics/brukercodes_DRIAMS-A_stasa_SA_EC_KP.csv | grep aureus | grep 2018 | cut -d',' -f4); do
+    for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*aureus*Oxacillin*480.json"); do
+        run "${MAIN} --input $code ${FILE}"
+    done
+done
+
+# K. pneumoniae jobs
+for code in $(cat /links/groups/borgwardt/Data/ms_diagnostics/brukercodes_DRIAMS-A_stasa_SA_EC_KP.csv | grep pneumoniae | grep 2018 | cut -d',' -f4); do
+    for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*pneu*Meropenem*480.json"); do
+      run "${MAIN} --input $code ${FILE}"
+    done
+done
+
+# E. coli jobs
+for code in $(cat /links/groups/borgwardt/Data/ms_diagnostics/brukercodes_DRIAMS-A_stasa_SA_EC_KP.csv | grep coli | grep 2018 | cut -d',' -f4); do
+    for FILE in $(find ../../results/fig4_curves_per_species_and_antibiotics/lr/ -name "*coli*Ceftriaxone*480.json"); do
+        run "${MAIN} --input $code ${FILE}"
+    done
+    echo $code
+done
