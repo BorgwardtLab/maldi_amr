1c648d9e0bf1623e1ea27e98d5f9cf34669896c3
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-12-16 10:56:51 +0100

    First working version (sort of) for sliding window validation

--- amr_maldi_ml/sliding_window_validation.py
+++ amr_maldi_ml/sliding_window_validation.py
@@ -9,6 +9,7 @@ import os
 import pathlib
 
 import numpy as np
+import pandas as pd
 
 from maldi_learn.driams import DRIAMSDatasetExplorer
 
@@ -34,6 +35,16 @@ class KeepAllBeforeFilter:
     def __call__(self, row):
         row_date = dateparser.parse(row['acquisition_date'])
         return row_date <= self.date
+
+
+class KeepRangeFilter:
+    def __init__(self, date_from, date_to):
+        self.date_from = dateparser.parse(date_from)
+        self.date_to = dateparser.parse(date_to)
+
+    def __call__(self, row):
+        row_date = dateparser.parse(row['acquisition_date'])
+        return self.date_from <= row_date <= self.date_to
 
 
 if __name__ == '__main__':
@@ -130,196 +141,95 @@ if __name__ == '__main__':
         [spectrum.intensities for spectrum in driams_dataset.X]
     )
 
-    date_filter = KeepAllBeforeFilter(date='2018-04-30')
+    # TODO: make configurable
+    test_from = '2018-04-30' 
 
+    date_filter = KeepAllBeforeFilter(date=test_from)
     mask = driams_dataset.y.apply(date_filter, axis=1)
     X_train = X[mask]
     y_train = driams_dataset.y[mask]
 
-    raise 'heck'
-
-    X_test = np.asarray(
-        [spectrum.intensities for spectrum in driams_dataset_test.X]
-    )
-    y_test = driams_dataset_test.to_numpy(args.antibiotic)
-
-    # Again, subset the data correctly so that we are never using any
-    # test data.
-    X_test = X_test[test_index]
-    y_test = y_test[test_index]
-
-    # Determine the class ration on the test data set
-    class_ratios = np.bincount(y_test) / len(y_test)
-    logging.info(f'Desired minority class ratio: {class_ratios[1]:.2f}')
-
-    # Check that we are in the right scenario; class 1 must be the
-    # minority class.
-    assert class_ratios[0] > class_ratios[1]
-
-    X_train = []
-    y_train = []
-
-    for train_year in train_years:
-        driams_dataset = load_driams_dataset(
-            DRIAMS_ROOT,
-            args.site,
-            train_year,
-            args.species,
-            antibiotics=args.antibiotic,
-            encoder=DRIAMSLabelEncoder(),
-            handle_missing_resistance_measurements='remove_if_all_missing',
-            spectra_type='binned_6000',
-        )
-
-        logging.info(f'Loaded training data for {train_year}')
-
-        X = np.asarray(
-            [spectrum.intensities for spectrum in driams_dataset.X]
-        )
-        y = driams_dataset.to_numpy(args.antibiotic)
-
-        # Remove the samples that we already used for testing; this
-        # ensures that there is no leakage.
-        #
-        # This procedure is not required for any other year because
-        # each year is treated individually.
-        if train_year == test_year:
-            X = X[train_index]
-            y = y[train_index]
-
-        # Again, check the correctness of this scenario; class 1 needs
-        # to be the minority class.
-        n0, n1 = np.bincount(y)
-        assert n0 > n1
-
-        desired_ratio = class_ratios[1] / class_ratios[0]
-
-        # Need random oversampling because the actual ratio is smaller
-        # than the desired ratio.
-        if n1 / n0 < desired_ratio:
-            ros = RandomOverSampler(
-                sampling_strategy=desired_ratio,
-                random_state=args.seed,
-            )
-
-            X, y = ros.fit_resample(X, y)
-
-        # Just pick the desired number of points at random and subset
-        # `X` and `y` accordingly.
-        else:
-            # Be generous with the number of points that we have to
-            # resample. This makes a difference of a single sample,
-            # so we should be good.
-            n_points_to_sample = int(desired_ratio * n0 + 0.5)
-
-            # The resampling only pertains to the positive, i.e. the
-            # minority, class.
-            indices = np.nonzero(y == 1)[0]
-
-            indices = resample(
-                indices,
-                n_samples=n_points_to_sample,
-                replace=False,
-                random_state=args.seed
-            )
-
-            # Add the remaining indices as well (of the majority class);
-            # we do not need to perform any resampling here.
-            indices = np.concatenate(
-                    (indices, np.nonzero(y == 0)[0])
-            )
-
-            X, y = X[indices], y[indices]
-
-        class_ratio = np.bincount(y)[1] / len(y)
-
-        logging.info(f'Achieved minority class ratio of {class_ratio:.2f} '
-                     f'for {train_year}')
-
-        X_train.extend(X)
-        y_train.extend(y)
-
-    X_train = np.asarray(X_train)
-    y_train = np.asarray(y_train)
-
-    if args.resample:
-        n_samples = len(train_index)
-
-        logging.info(
-            f'Attempting to resample `X_train` and `y_train` to {n_samples}'
-        )
-
-        if n_samples > len(X_train):
-            logging.warning(f'Cannot perform subsampling because only '
-                            f'{len(X_train)} samples are available.'
-            )
-        else:
-            X_train, y_train = resample(
-                X_train, y_train,
-                n_samples=n_samples,
-                replace=False,
-                stratify=y_train,
-                random_state=args.seed,
-            )
-
-    class_ratio = np.bincount(y_train)[1] / len(y_train)
-    logging.info(f'Achieved overall minority class ratio of {class_ratio:.2f}')
-
-    # The shuffling is technically only required for the `train` data
-    # set because we expect this to contain multiple years. We make a
-    # better effort, however, in order to be prepared for anything.
-    X_train, y_train = shuffle(X_train, y_train, random_state=args.seed)
-    X_test, y_test = shuffle(X_test, y_test, random_state=args.seed)
-
-    # Prepare the output dictionary containing all information to
-    # reproduce the experiment.
-    output = {
-        'site': args.site,
-        'train_years': train_years,
-        'test_years': [test_year],        # required for compatibility
-        'species': args.species,
-        'seed': args.seed,
-        'model': args.model,
-        'antibiotic': args.antibiotic,
-        'resample': args.resample,        # to be able to know what is going on
-        'n_samples_train': len(X_train),  # potentially useful
-        'n_samples_test': len(X_test),    # ditto
-    }
-
-    # Add fingerprint information about the metadata files to make sure
-    # that the experiment is reproducible.
-    output['metadata_versions'] = metadata_fingerprints
-
-    suffix = None if not args.resample else 'resample'
-
-    output_filename = generate_output_filename(
-        args.output,
-        output,
-        suffix=suffix
+    X_test = X[~mask]
+    y_test = np.asarray(
+        driams_dataset.y[~mask][args.antibiotic].values,
+        dtype=int
     )
 
-    # Only write if we either are running in `force` mode, or the
-    # file does not yet exist.
-    if not os.path.exists(output_filename) or args.force:
+    date_range = pd.date_range(
+        start='2015-11-01',
+        end='2018-04-30',
+        freq='M',
+    )
 
-        n_folds = 5
+    df = pd.DataFrame(index=date_range)
+    for i, window in enumerate(df.rolling(6)):
+        # Ignore first periods that do not contain sufficient data.
+        if i <= 4:
+            continue
 
-        results = run_experiment(
-            X_train, y_train,
-            X_test, y_test,
-            args.model,
-            n_folds,
-            random_state=args.seed,  # use seed whenever possible
-            verbose=True             # want info about best model etc.
+        date_from = window.index[0].strftime('%Y-%m-%d')
+        date_to = window.index[-1].strftime('%Y-%m-%d')
+
+        date_filter = KeepRangeFilter(date_from, date_to)
+        mask = y_train.apply(date_filter, axis=1)
+
+        # Local data sets to use for training at this range.
+        X_train_ = X_train[mask]
+        y_train_ = np.asarray(
+            y_train[mask][args.antibiotic].values,
+            dtype=int
         )
 
-        output.update(results)
+        # Prepare the output dictionary containing all information to
+        # reproduce the experiment.
+        output = {
+            'site': args.site,
+            'years': '*',
+            'species': args.species,
+            'seed': args.seed,
+            'model': args.model,
+            'antibiotic': args.antibiotic,
+            'train_from': date_from,
+            'train_to': date_to,
+            'test_from': test_from,
+        }
 
-        logging.info(f'Saving {os.path.basename(output_filename)}')
+        # Add fingerprint information about the metadata files to make sure
+        # that the experiment is reproducible.
+        output['metadata_versions'] = metadata_fingerprints
 
-        with open(output_filename, 'w') as f:
-            json.dump(output, f, indent=4)
-    else:
-        logging.warning(
-            f'Skipping {output_filename} because it already exists.'
+        suffix = f'{date_from}_{date_to}'
+
+        output_filename = generate_output_filename(
+            args.output,
+            output,
+            suffix=suffix
         )
+
+        # Only write if we either are running in `force` mode, or the
+        # file does not yet exist.
+        if not os.path.exists(output_filename) or args.force:
+
+            n_folds = 5
+
+            results = run_experiment(
+                # Use local variants of the train data set because they
+                # have been subset to a range of months.
+                X_train_, y_train_,
+                X_test, y_test,
+                args.model,
+                n_folds,
+                random_state=args.seed,  # use seed whenever possible
+                verbose=True             # want info about best model etc.
+            )
+
+            output.update(results)
+
+            logging.info(f'Saving {os.path.basename(output_filename)}')
+
+            with open(output_filename, 'w') as f:
+                json.dump(output, f, indent=4)
+        else:
+            logging.warning(
+                f'Skipping {output_filename} because it already exists.'
+            )
