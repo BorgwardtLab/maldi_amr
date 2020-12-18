16b1665e2eb99024f362b4c7ed49eb94e3202296
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-12-18 09:17:03 +0100

    Made metric and date column configurable

--- amr_maldi_ml/plot_sliding_window_validation.py
+++ amr_maldi_ml/plot_sliding_window_validation.py
@@ -12,8 +12,25 @@ import seaborn as sns
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('INPUT', type=str, nargs='+', help='Input file(s)')
+    parser.add_argument(
+        '-m', '--metric',
+        type=str,
+        default='auroc',
+        help='Metric to use for plotting'
+    )
+
+    parser.add_argument(
+        '-d', '--date-column',
+        type=str,
+        default='train_to',
+        help='Date column to use for visualisation'
+    )
 
     args = parser.parse_args()
+
+    # Create new data frame by concatenating all the JSON files there
+    # are. This assumes that the fields are the same in all files. It
+    # will inevitably *fail* if this is not the case.
 
     rows = []
 
@@ -24,26 +41,33 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(rows)
 
+    # Convert all columns to dates. This simplifies the `seaborn`
+    # handling below as it will recognise the special structure of
+    # the data frame.
+
     for column in df.columns:
-        if '_to' in column or '_from' in column:
+        if column.endswith('_to') or column.endswith('_from'):
             df[column] = pd.to_datetime(df[column])
 
-    df = df.sort_values('train_to')
-    df['auroc'] *= 100
+    df = df.sort_values(args.date_column)
+    df[args.metric] *= 100
 
+    # Some debug output, just so all values can be seen in all their
+    # glory.
     print(
-        df.groupby(['train_to', 'species', 'antibiotic']).agg(
+        df.groupby([args.date_column, 'species', 'antibiotic']).agg(
             {
-                'auroc': [np.mean, np.std]
+                args.metric: [np.mean, np.std]
             }
         )
     )
 
+    # Create new column that describes the whole scenario.
     df['scenario'] = df['species'] + ' (' + df['antibiotic'] + ')'
 
     g = sns.lineplot(
-        x='train_to',
-        y='auroc',
+        x=args.date_column,
+        y=args.metric,
         data=df,
         hue='scenario'
     )
