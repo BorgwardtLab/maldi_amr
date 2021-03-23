16cade119e19c54bb99069d3294b09670c86734f
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-23 19:07:47 +0100

    Updated visualisation of sliding window experiment

--- amr_maldi_ml/plot_sliding_window_validation.py
+++ amr_maldi_ml/plot_sliding_window_validation.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
         x=args.date_column,
         y=args.metric,
         data=df,
-        hue=f'scenario ({model})'
+        hue=f'scenario ({model})',
     )
 
     if args.suffix != '':
@@ -97,8 +97,7 @@ if __name__ == '__main__':
 
     filename = f'plots/sliding_window/Model_{model}_Date_{args.date_column}_Metric_{args.metric}{suffix}.png'
 
-
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig(filename)
-    #plt.show()
+    plt.show()
