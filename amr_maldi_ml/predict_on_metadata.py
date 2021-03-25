3b31b3220771f0b5bb09cdae651a094f43126aa1
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-25 09:55:34 +0100

    Improving metadata prediction script

--- amr_maldi_ml/predict_on_metadata.py
+++ amr_maldi_ml/predict_on_metadata.py
@@ -1,6 +1,7 @@
 """Perform predictions based on metadata instead of spectra."""
 
 import argparse
+import logging
 
 import numpy as np
 import pandas as pd
@@ -13,7 +14,82 @@ from sklearn.metrics import classification_report
 from maldi_learn.driams import DRIAMSLabelEncoder
 
 
+def make_dataframes(filename, args):
+    """Create data frames from file, based on mode."""
+    df = pd.read_csv(
+        args.FILE,
+        sep=';',
+        na_values='-',
+        keep_default_na=True,
+        # Required to ensure that different data types can be inferred
+        # for the columns.
+        low_memory=False
+    )
+
+    # Get only the species we are interested in.
+    df = df.query('`Organism.best.match.` == @args.species')
+
+    # Retrieve all metadata columns. Since the file format differs
+    # between sites, we cannot rely on the ordering. However, each
+    # site has the same 21 columns, and the last 6 columns store a
+    # bunch of other data.
+    n_columns = len(df.columns)
+    metadata_columns = df.columns[np.r_[:21, n_columns-6:n_columns]]
+
+    # Split the data set into two parts: one containing metadata
+    # columns, the other one only containing resistance info. We
+    # do a quick sanity check here.
+    df_metadata = df[metadata_columns]
+    df_resistance = df.drop(columns=metadata_columns)
+
+    # Get all potential resistance values, but ignore all NaNs during
+    # the set construction.
+    resistance_values = set(df_resistance.values.ravel())
+    resistance_values = set(filter(lambda x: x == x , resistance_values))
+
+    assert 'I' in resistance_values
+    assert 'R' in resistance_values
+
+    encoder = DRIAMSLabelEncoder()
+
+    # Only use a specific antibiotic for the prediction task. This could
+    # also be achieved in the query above.
+    df_resistance = encoder.fit_transform(df_resistance)
+    df_resistance = df_resistance[args.antibiotic]
+
+    # Remove all values of the prediction data frame that contain NaNs
+    # because we cannot assign them a proper label.
+    df_metadata = df_metadata[~df_resistance.isna()]
+    df_resistance = df_resistance[~df_resistance.isna()]
+
+    # TODO: we remove all NaN values from the metadata because it is
+    # unclear how to convert them.
+    #
+    # Need to drop NaN before converting to `numpy` data frame. We could
+    # probably be smarter here.
+    df_metadata = df_metadata.dropna(axis='columns')
+
+    df_metadata = df_metadata.drop(
+        columns=[
+            c for c in df_metadata.columns if c.endswith('_id')
+        ]
+    )
+
+    # TODO: make this configurable
+    if False:
+        X = df_metadata.select_dtypes(include=[np.number])
+    else:
+        X = pd.get_dummies(df_metadata).to_numpy()
+
+
 if __name__ == '__main__':
+    # Basic log configuration to ensure that we see where the process
+    # spends most of its time.
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(message)s'
+    )
+
     parser = argparse.ArgumentParser()
     parser.add_argument('FILE')
     parser.add_argument(
@@ -26,8 +102,17 @@ if __name__ == '__main__':
         type=str,
         default='Escherichia coli'
     )
+    parser.add_argument(
+        '-m', '--mode',
+        type=str,
+        choices=['numerical', 'categorical', 'all'],
+        help='Choice of variable pre-selection',
+        default='numerical'
+    )
 
     args = parser.parse_args()
+
+    make_dataframes(args.FILE, args)
 
     df = pd.read_csv(
         args.FILE,
@@ -38,8 +123,13 @@ if __name__ == '__main__':
     )
     df = df.query('`Organism.best.match.` == @args.species')
 
-    metadata_columns = df.columns[np.r_[:21, 102:108]]
+    n_columns = len(df.columns)
+    metadata_columns = df.columns[np.r_[:21, n_columns-6:n_columns]]
     df_metadata = df[metadata_columns]
+
+    print(metadata_columns.values)
+    raise 'crap'
+
     df_resistance = df.drop(columns=metadata_columns)
 
     encoder = DRIAMSLabelEncoder()
