3103a4aa4ed9bc2c0d9fdffe238f97483d851ac2
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-03-26 13:38:57 +0100

    rename files to not have acqu suffix

--- amr_maldi_ml/DRIAMS_preprocessing/id_files/acquisition_dates/run_strat_analysis.sh
+++ amr_maldi_ml/DRIAMS_preprocessing/id_files/acquisition_dates/run_strat_analysis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # DRIAMS-A clean stratification IDRES files
-python id_file_with_strat_acqu.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_date/2018_01-08_IDRES_AB_not_summarised.csv \
-                        ./2018_strat_acqu.csv
-python id_file_with_strat_acqu.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_date/2017-01-12_IDRES_AB_not_summarised.csv \
-                        ./2017_strat_acqu.csv
-python id_file_with_strat_acqu.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_date/2016-01-12_IDRES_AB_not_summarised.csv \
-                        ./2016_strat_acqu.csv
-python id_file_with_strat_acqu.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_date/2015-01-12_IDRES_AB_not_summarised.csv \
-                        ./2015_strat_acqu.csv
-python ./remove_overlap_17_18_strat_acqu.py
-python ./remove_missing_spectra_17_strat_acqu.py
+python id_file_with_strat.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_add_workstation/2018_01-08_IDRES_AB_not_summarised.csv \
+                        ./2018_strat.csv
+python id_file_with_strat.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_add_workstation/2017-01-12_IDRES_AB_not_summarised.csv \
+                        ./2017_strat.csv
+python id_file_with_strat.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_add_workstation/2016-01-12_IDRES_AB_not_summarised.csv \
+                        ./2016_strat.csv
+python id_file_with_strat.py /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/ac_add_workstation/2015-01-12_IDRES_AB_not_summarised.csv \
+                        ./2015_strat.csv
+python ./remove_overlap_17_18_strat.py
+python ./remove_missing_spectra_17_strat.py
 
 # DRIAMS-A print some summary statistics for consistency checks
 #python check_consistency_strat_data.py --prev /links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/codeAC/USB/2018/2018_01-08_IDRES_AB_not_summarised.csv \
