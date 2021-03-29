36b768ae550eea9f1c28781cd80e157f0ffbbfcf
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-29 18:13:10 +0200

    New submission script with filter expressions

---
+++ amr_maldi_ml/scripts/submit_curves_filtered.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Main command to execute for all combinations created in the script
+# below. The space at the end of the string is important.
+MAIN="poetry run python ../curves_per_species_and_antibiotic.py --force "
+
+# Try to be smart: if `bsub` does *not* exist on the system, we just
+# pretend that it is an empty command.
+if [ -x "$(command -v bsub)" ]; then
+  BSUB='bsub -W 23:59 -o "curves_per_species_and_antibiotic_%J.out" -R "rusage[mem=16000]"'
+fi
+
+# Evaluates its first argument either by submitting a job, or by
+# executing the command without parallel processing.
+run() {
+  if [ -z "$BSUB" ]; then
+    eval "$1";
+  else
+    eval "${BSUB} $1";
+  fi
+}
+
+function make_jobs {
+  local SEED=${1}
+  local FILTER=${2}
+
+  # S. aureus jobs
+  CMD="${MAIN} --antibiotic Oxacillin --species \"Staphylococcus aureus\" --seed $SEED $FILTER"
+  run "$CMD";
+
+  # E. coli and K. pneumoniae jobs
+  for SPECIES in 'Escherichia coli' 'Klebsiella pneumoniae'; do
+    CMD="${MAIN} --antibiotic Ceftriaxone --species \"$SPECIES\" --seed $SEED $FILTER"
+    run "$CMD";
+  done
+}
+
+# The grid is kept sparse for now. This is *not* an inconsistency.
+for SEED in 344 172 188 270 35 164 545 480 89 409; do
+  make_jobs $SEED
+  make_jobs $SEED "-F \"workstation == HospitalHygiene\""
+  make_jobs $SEED "-F \"workstation != HospitalHygiene\""
+done
