3719bde2445962606009fc3542eb4e2eb0ac5ed4
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-04-09 17:06:45 +0200

    New submission script for per-workstation resampling

---
+++ amr_maldi_ml/scripts/submit_resample_per_workstation.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Main command to execute for all combinations created in the script
+# below. The space at the end of the string is important.
+MAIN="poetry run python ../resample_per_workstation.py --force "
+
+# Try to be smart: if `bsub` does *not* exist on the system, we just
+# pretend that it is an empty command.
+if [ -x "$(command -v bsub)" ]; then
+  BSUB='bsub -W 23:59 -o "resample_per_workstation_%J.out" -R "rusage[mem=16000]"'
+fi
+
+# Evaluates its first argument either by submitting a job, or by
+# executing the command without parallel processing.
+run() {
+  if [ -z "$BSUB" ]; then
+    eval "$1";
+  else
+    eval "${BSUB} $1";
+  fi
+}
+
+function make_jobs {
+  local SEED=${1}
+  local WORKSTATION=${2}
+
+  # S. aureus jobs
+  CMD="${MAIN} --antibiotic Oxacillin --species \"Staphylococcus aureus\" --seed $SEED --workstation $WORKSTATION"
+  run "$CMD";
+
+  # E. coli and K. pneumoniae jobs
+  for SPECIES in 'Escherichia coli' 'Klebsiella pneumoniae'; do
+    CMD="${MAIN} --antibiotic Ceftriaxone --species \"$SPECIES\" --seed $SEED --workstation $WORKSTATION"
+    run "$CMD";
+  done
+}
+
+# The grid is kept sparse for now. This is *not* an inconsistency.
+for SEED in 344 172 188 270 35 164 545 480 89 409; do
+  make_jobs $SEED "HospitalHygiene"
+  make_jobs $SEED "Blood"
+  make_jobs $SEED "DeepTissue"
+  make_jobs $SEED "Urine"
+done
