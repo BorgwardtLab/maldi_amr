5e427601d72e6813a11b29d03dae5c53a74cc3c5
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-04-13 19:09:26 +0200

    Simplified submission script for validation jobs

--- amr_maldi_ml/scripts/submit_validation_per_species_and_antibiotic.sh
+++ amr_maldi_ml/scripts/submit_validation_per_species_and_antibiotic.sh
@@ -9,12 +9,12 @@
 
 # Main command to execute for all combinations created in the script
 # below. The space at the end of the string is important.
-MAIN="poetry run python ../validation_per_species_and_antibiotic.py "
+MAIN="poetry run python ../validation_per_species_and_antibiotic.py --force "
 
 # Try to be smart: if `bsub` does *not* exist on the system, we just
 # pretend that it is an empty command.
 if [ -x "$(command -v bsub)" ]; then
-  BSUB='bsub -W 23:59 -o "validation_per_species_and_antibiotic_%J.out" -R "rusage[mem=32000]"'
+  BSUB='bsub -W 23:59 -o "validation_per_species_and_antibiotic_%J.out" -R "rusage[mem=16000]"'
 fi
 
 # Evaluates its first argument either by submitting a job, or by
@@ -29,23 +29,20 @@ run() {
 
 for SEED in 344 172 188 270 35 164 545 480 89 409; do
   for TRAIN in "DRIAMS-A" "DRIAMS-B" "DRIAMS-C" "DRIAMS-D"; do
-    # Switching the order for the submission around because we are
-    # primarily interested in obtaining information about unknown
-    # sites quickly.
-    for TEST in "DRIAMS-B" "DRIAMS-C" "DRIAMS-D" "DRIAMS-A"; do
+    for TEST in "DRIAMS-B" "DRIAMS-C" "DRIAMS-D"; do
+      for MODEL in "lr"; do 
 
-      # Models are ordered by their 'utility' for the project. We are
-      # most interested in logistic regression.
-      for MODEL in "lr" "svm-rbf" "rf" "lightgbm" "svm-linear"; do
-
-        # E. coli and K. pneu
-        for ANTIBIOTIC in "Ceftriaxone" "Ciprofloxacin"; do
+        # E. coli and K. pneu jobs
+        for ANTIBIOTIC in "Ceftriaxone"; do
           for SPECIES in "Escherichia coli" "Klebsiella pneumoniae"; do
               CMD="${MAIN} --train-site $TRAIN --test-site $TEST --species \"$SPECIES\" --antibiotic \"$ANTIBIOTIC\" --model $MODEL --seed $SEED"
               run "$CMD";
           done # species
         done # E. coli and K. pneu
 
+        # S. aureus jobs
+        CMD="${MAIN} --train-site $TRAIN --test-site $TEST --antibiotic Oxacillin --species \"Staphylococcus aureus\" --model $MODEL --seed $SEED"
+        run "$CMD";
       done # models
     done # test
   done # train
