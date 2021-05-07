0e9e6873b5da2ccd659f2580e7767f61b936fa25
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-07 13:33:55 +0200

    add importance weighting script

---
+++ amr_maldi_ml/domain_adaptation_shallow_iw.py
@@ -0,0 +1,260 @@
+"""Domain adaptation for shallow (i.e. non-deep) models."""
+
+import argparse
+import dotenv
+import json
+import logging
+import os
+import pathlib
+
+import numpy as np
+
+from importance_weighting import run_experiment
+
+from sklearn.preprocessing import StandardScaler
+
+from maldi_learn.driams import DRIAMSDatasetExplorer
+from maldi_learn.filters import DRIAMSBooleanExpressionFilter
+
+from maldi_learn.driams import load_driams_dataset
+
+from maldi_learn.utilities import case_based_stratification
+from maldi_learn.utilities import stratify_by_species_and_label
+
+from utilities import generate_output_filename
+
+from models import calculate_metrics
+
+dotenv.load_dotenv()
+DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
+
+
+def _load_data(
+    site,
+    years,
+    species,
+    antibiotic,
+    seed):
+
+    """Load data set and return it in partitioned form."""
+    extra_filters = []
+    if site == 'DRIAMS-A':
+        extra_filters.append(
+            DRIAMSBooleanExpressionFilter('workstation != HospitalHygiene')
+        )
+
+    id_suffix = 'clean'
+    strat_fn = stratify_by_species_and_label
+
+    if site == 'DRIAMS-A':
+        id_suffix = 'strat'
+        strat_fn = case_based_stratification
+
+    driams_dataset = load_driams_dataset(
+        DRIAMS_ROOT,
+        site,
+        '*',
+        species=species,
+        antibiotics=antibiotic,
+        handle_missing_resistance_measurements='remove_if_all_missing',
+        spectra_type='binned_6000',
+        on_error='warn',
+        id_suffix=id_suffix,
+        extra_filters=extra_filters,
+    )
+
+    logging.info(f'Loaded data set for {species} and {antibiotic}')
+
+    X = np.asarray([spectrum.intensities for spectrum in driams_dataset.X])
+
+    logging.info('Finished vectorisation')
+
+    # Stratified train--test split
+    train_index, test_index = strat_fn(
+        driams_dataset.y,
+        antibiotic=antibiotic,
+        random_state=seed,
+    )
+
+    logging.info('Finished stratification')
+
+    # Use the column containing antibiotic information as the primary
+    # label for the experiment. All other columns will be considered
+    # metadata. The remainder of the script decides whether they are
+    # being used or not.
+    y = driams_dataset.to_numpy(antibiotic)
+    meta = driams_dataset.y.drop(columns=antibiotic)
+
+    X_train, y_train = X[train_index], y[train_index]
+    X_test, y_test = X[test_index], y[test_index]
+    meta_train, meta_test = meta.iloc[train_index], meta.iloc[test_index]
+
+    return X_train, y_train, X_test, y_test, meta_train, meta_test
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-a', '--antibiotic',
+        default='Ceftriaxone',
+        type=str,
+        help='Antibiotic for which to run the experiment',
+    )
+
+    parser.add_argument(
+        '-S', '--seed',
+        type=int,
+        help='Random seed to use for the experiment',
+        required=True
+    )
+
+    parser.add_argument(
+        '-m', '--model',
+        default='lr',
+        help='Selects model to use for subsequent training'
+    )
+
+    parser.add_argument(
+        '-s', '--species',
+        default='Escherichia coli',
+        type=str,
+        help='Species for which to run the experiment',
+    )
+
+    name = 'domain_adaptation_shallow'
+
+    parser.add_argument(
+        '-o', '--output',
+        default=pathlib.Path(__file__).resolve().parent.parent / 'results'
+                                                               / name,
+        type=str,
+        help='Output path for storing the results.'
+    )
+
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='If set, overwrites all files. Else, skips existing files.'
+    )
+
+    parser.add_argument(
+        '--source-site',
+        type=str,
+        help='Site to use as source (e.g. DRIAMS-A)',
+        required=True
+    )
+
+    parser.add_argument(
+        '--target-site',
+        type=str,
+        help='Site to use as target (e.g. DRIAMS-B)',
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    # Create the output directory for storing all results of the
+    # individual combinations.
+    os.makedirs(args.output, exist_ok=True)
+
+    # Basic log configuration to ensure that we see where the process
+    # spends most of its time.
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(message)s'
+    )
+
+    source_site = args.source_site
+    target_site = args.target_site
+
+    explorer = DRIAMSDatasetExplorer(DRIAMS_ROOT)
+    metadata_fingerprints_source = explorer.metadata_fingerprints(source_site)
+    metadata_fingerprints_target = explorer.metadata_fingerprints(target_site)
+
+    source_years = explorer.available_years(source_site)
+    target_years = explorer.available_years(target_site)
+
+    logging.info(f'Source site: {source_site}')
+    logging.info(f'Source years: {source_years}')
+    logging.info(f'Target site: {target_site}')
+    logging.info(f'Target years: {target_years}')
+    logging.info(f'Seed: {args.seed}')
+    logging.info(f'Antibiotic: {args.antibiotic}')
+    logging.info(f'Species: {args.species}')
+
+    # TODO: we are losing some samples here because we ignore the
+    # `X_test` split; on the other hand, this is compatible  with
+    # our validation scenario(s).
+    X_train, y_train, X_test, y_test, *_ = _load_data(
+        args.source_site,
+        source_years,
+        args.species,
+        args.antibiotic,
+        args.seed,
+    )
+
+    logging.info('Loaded source site data')
+
+    # We don't need `z_train`, i.e. the labels on the training domain,
+    # because we never look at them anyway.
+    Z_train, z_train, Z_test, z_test, *_ = _load_data(
+        args.target_site,
+        target_years,
+        args.species,
+        args.antibiotic,
+        args.seed,
+    )
+
+    logging.info('Loaded target site data')
+
+    # Prepare the output dictionary containing all information to
+    # reproduce the experiment.
+    output = {
+        'train_site': source_site,
+        'source_years': source_years,
+        'test_site': target_site,
+        'target_years': target_years,
+        'seed': args.seed,
+        'antibiotic': args.antibiotic,
+        'species': args.species,
+        'model': 'ImportanceWeightedClassifier',  # TODO: Make adjustable
+    }
+
+    # Add fingerprint information about the metadata files to make sure
+    # that the experiment is reproducible.
+    output['metadata_versions_source'] = metadata_fingerprints_source
+    output['metadata_versions_target'] = metadata_fingerprints_target
+
+    output_filename = generate_output_filename(
+        args.output,
+        output,
+    )
+
+    # Only write if we either are running in `force` mode, or the
+    # file does not yet exist.
+    if not os.path.exists(output_filename) or args.force:
+
+        n_folds = 5
+        
+        results = run_experiment(
+                X_train, y_train,
+                X_test, y_test,
+                Z_train, z_train,
+                Z_test, z_test,
+                model=args.model,
+                n_folds=n_folds,
+                random_state=args.seed,
+        )
+
+        output.update(results)
+
+        logging.info(f'Saving {os.path.basename(output_filename)}')
+
+        with open(output_filename, 'w') as f:
+            json.dump(output, f, indent=4)
+    else:
+        logging.warning(
+            f'Skipping {output_filename} because it already exists.'
+        )
---
+++ amr_maldi_ml/importance_weighting.py
@@ -0,0 +1,209 @@
+"""Describes models for subsequent training."""
+
+import logging
+import joblib
+import json
+import warnings
+
+import numpy as np
+
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from models import get_pipeline_and_parameters
+from models import load_pipeline
+from models import calculate_metrics
+
+
+def run_experiment(
+    X_train, y_train,
+    X_test, y_test,
+    Z_train, z_train,
+    Z_test, z_test,
+    model,
+    n_folds,
+    random_state=None,
+    verbose=False,
+    scoring='roc_auc',
+    class_weight='balanced',
+    meta_train=None,
+    meta_test=None,
+):
+    """Run experiment for given train--test split.
+
+    This is the main function for training and testing a classifier. The
+    function is oblivious to the context of the training. It performs an
+    in-depth grid search and evaluates the results.
+
+    Parameters
+    ----------
+    X_train : array-like
+        Training data
+
+    y_train : list
+        Labels for the training data
+
+    X_test : array_like
+        Test data
+
+    y_test : list
+        Labels for the rest data
+
+    model : str
+        Specifies a model whose pipeline will be queried and set up by
+        this function. Must be a valid model according to the function
+        `get_pipeline_and_parameters()`.
+
+    n_folds : int
+        Number of folds for internal cross-validation
+
+    random_state : int, `RandomState` instance, or None
+        If set, propagates random state to a model. This is *not*
+        required or used for all models.
+
+    scoring : str, optional
+        Specifies scoring function to use. Should be a string that can
+        be understood by `GridSearchCV`.
+
+    verbose : bool, optional
+        If set, will add verbose information about the trained model in
+        the form of adding the best parameters as well as information
+        about predicted labels and scores.
+
+    meta_train : `pd.DataFrame`, optional
+        If set, will add additional information about training samples
+        based on metadata. Only applies if `verbose = True`.
+
+    meta_test : `pd.DataFrame`, optional
+        If set, will add additional information about test samples based
+        on metadata. Only applies if `verbose = True`.
+
+    Returns
+    -------
+    A dictionary containing measurement descriptions and their
+    corresponding values.
+    """
+    pipeline, param_grid = get_pipeline_and_parameters(
+        model,
+        random_state,
+        class_weight=class_weight,
+    )
+
+    grid_search = GridSearchCV(
+        pipeline,
+        param_grid=param_grid,
+        cv=n_folds,
+        scoring=scoring,
+        n_jobs=-1,
+    )
+
+    # Construct domain classification dataset 
+    n, dim_X = X_train.shape 
+    m, dim_Z = Z_train.shape
+    assert dim_X == dim_Z, 'Dimensionalities do not match between X and Z.'
+
+    XZ_train = np.concatenate((X_train, Z_train), axis=0)
+    classes = np.r_[(np.zeros(n), np.ones(m))]
+    
+    logging.info(f'Starting grid search for importance classifiers on {len(XZ_train)} training samples')
+
+    # Ignore these warnings only for the grid search process. The
+    # reason is that some of the jobs will inevitably *fail* to
+    # converge because of bad `C` values. We are not interested in
+    # them anyway.
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=ConvergenceWarning)
+        warnings.filterwarnings('ignore', category=UserWarning)
+
+        with joblib.parallel_backend('threading', -1):
+            grid_search.fit(XZ_train, classes)
+
+    # Calculate importance weighting
+    X_importance_weights = grid_search.predict_proba(X_train)
+    print(X_importance_weights.min())
+    print(X_importance_weights.max())
+
+    # Ignore these warnings only for the grid search process. The
+    # reason is that some of the jobs will inevitably *fail* to
+    # converge because of bad `C` values. We are not interested in
+    # them anyway.
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=ConvergenceWarning)
+        warnings.filterwarnings('ignore', category=UserWarning)
+
+        with joblib.parallel_backend('threading', -1):
+            grid_search.fit(X_train, y_train, 
+                            sample_weight=X_importance_weights,
+                            )
+
+    # Calculate metrics for the training data fully in-line because we
+    # only ever want to save the results.
+    train_metrics = calculate_metrics(
+        y_train,
+        grid_search.predict(X_train),
+        grid_search.predict_proba(X_train),
+        prefix='train'
+    )
+
+    y_pred = grid_search.predict(X_test)
+    y_score = grid_search.predict_proba(X_test)
+
+    test_metrics = calculate_metrics(y_test, y_pred, y_score)
+
+    # Replace information about the standard scaler prior to writing out
+    # the `best_params_` grid. The reason for this is that we cannot and
+    # probably do not want to serialise the scaler class. We only need
+    # to know *if* a scaler has been employed.
+    if 'scaler' in grid_search.best_params_:
+        scaler = grid_search.best_params_['scaler']
+        if scaler != 'passthrough':
+            grid_search.best_params_['scaler'] = type(scaler).__name__
+
+    # Prepare the results dictionary for this experiment. Depending on
+    # the input parameters of this function, additional information is
+    # added.
+    results = {
+        'scoring': scoring,
+    }
+
+    # Add additional information about split. This is relevant for
+    # scenarios with a case-based stratification.
+    results.update({
+        'test_size_obtained': len(y_test) / (len(y_train) + len(y_test)),
+        'prevalence_train': (np.bincount(y_train) / len(y_train)).tolist(),
+        'prevalence_test': (np.bincount(y_test) / len(y_test)).tolist(),
+    })
+
+    if verbose:
+        results.update({
+            'best_params': grid_search.best_params_,
+            'y_score': y_score.tolist(),
+            'y_pred': y_pred.tolist(),
+            'y_test': y_test.tolist(),
+        })
+
+        # Only include certain columns of the meta data. This is
+        # future-proof in case we need more columns or more data
+        # later on.
+        meta_columns = ['workstation']
+
+        for col in meta_columns:
+            if meta_train is not None and col in meta_train:
+                results.update({
+                    f'meta_train_{col}': meta_train[col].values.tolist(),
+                })
+            if meta_test is not None and col in meta_test:
+                results.update({
+                    f'meta_test_{col}': meta_test[col].values.tolist()
+                })
+
+    # Add information that *always* needs to be available.
+    results.update(train_metrics)
+    results.update(test_metrics)
+
+    return results
