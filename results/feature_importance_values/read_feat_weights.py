d46e8ee0d358f1e13fb9d069243584b86a00382d
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-14 13:42:49 +0200

    update to new calculate_feature_importance_values script

--- results/feature_importance_values/read_feat_weights.py
+++ results/feature_importance_values/read_feat_weights.py
@@ -1,30 +1,41 @@
+import argparse
 import json
 
 import numpy as np
 import pandas as pd
 
 
-filenames = [
-    'Site_DRIAMS-A_Model_lr_Species_Escherichia_coli_Antibiotic_Ceftriaxone_Seed_164-172-188-270-344-35-409-480-545-89_average.json',
-    'Site_DRIAMS-A_Model_lr_Species_Staphylococcus_aureus_Antibiotic_Oxacillin_Seed_164-172-188-270-344-35-409-480-545-89_average.json',
-    'Site_DRIAMS-A_Model_lr_Species_Klebsiella_pneumoniae_Antibiotic_Meropenem_Seed_164-172-188-270-344-35-409-480-545-89_average.json',
-]
+def json_to_csv(args):
+    for filename in args.INPUT:
+        with open(filename) as f:
+            data = json.load(f)
+
+        df = pd.DataFrame({k: data[k] for k in ['mean_feature_weights', 'std_feature_weights']})
+
+        rank = [sorted(
+           -np.abs(df['mean_feature_weights'])
+                       ).index(x) for x in -np.abs(df['mean_feature_weights'])
+                ]
+        df['rank'] = [r+1 for r in rank]
+
+        df['bin_start'] = np.linspace(2000, 19997, 6000).astype(int).tolist()
+        df['bin_end'] = np.linspace(2003, 20000, 6000).astype(int).tolist()
+
+        outname = filename.replace('.json', '.csv')
+        df.to_csv(outname, index=False)
 
 
-for filename in filenames:
-    with open(filename) as f:
-        data = json.load(f)
+if __name__ == '__main__':
 
-    df = pd.DataFrame({k: data[k][0] for k in ['mean_feature_importance', 'std_feature_importance']})
+    parser = argparse.ArgumentParser()
 
-    rank = [sorted(
-       -np.abs(df['mean_feature_importance'])
-                   ).index(x) for x in -np.abs(df['mean_feature_importance'])
-            ]
-    df['rank'] = [r+1 for r in rank]
-
-    df['bin_start'] = np.linspace(2000, 19997, 6000).astype(int).tolist()
-    df['bin_end'] = np.linspace(2003, 20000, 6000).astype(int).tolist()
-
-    outname = filename.replace('.json', '.csv')
-    df.to_csv(outname, index=False)
+    parser.add_argument(
+        'INPUT',
+        type=str,
+        help='Input file',
+        nargs='+',
+    )
+    
+    args = parser.parse_args()
+    
+    json_to_csv(args)
