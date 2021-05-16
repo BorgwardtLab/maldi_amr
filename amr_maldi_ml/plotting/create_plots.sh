94f245619eac151dbef132e92d6308f12499190f
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-16 16:23:43 +0200

    update plots

--- amr_maldi_ml/plotting/create_plots.sh
+++ amr_maldi_ml/plotting/create_plots.sh
@@ -3,8 +3,7 @@
 # ------------
 # Figure 2: barplot and vmeplot
 # ------------
-#antibiotic_list_small=("Ciprofloxacin,Amoxicillin-Clavulanic acid,Ceftriaxone,Oxacillin,Tigecycline,Colistin,Fluconazole")
-antibiotic_list_small=("Ciprofloxacin,Ceftriaxone,Oxacillin,Tigecycline,Colistin,Fluconazole")
+antibiotic_list_small=("Ciprofloxacin,Amoxicillin-Clavulanic acid,Ceftriaxone,Oxacillin,Tigecycline,Colistin,Fluconazole")
 echo "$antibiotic_list_small"
 
 python plot_baseline_barplot.py --antibiotic "$antibiotic_list_small" \
