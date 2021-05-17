73a17198d0c62671dffade09e7c22d95cc2ef3be
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-17 15:58:35 +0200

    add all scenarios

--- amr_maldi_ml/building/create_table.sh
+++ amr_maldi_ml/building/create_table.sh
@@ -2,3 +2,5 @@
 # Table: Rejection ratio tables
 # ------------
 python build_assymetric_rejection.py ../../results/calibrated_classifiers/lr 
+python build_assymetric_rejection.py ../../results/calibrated_classifiers/lightgbm 
+python build_assymetric_rejection.py ../../results/calibrated_classifiers/mlp 
