5b3e7193b691e177e6050002bba33c4a18f3cb8f
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-18 15:29:48 +0200

    adjust to include mlp and oversampling

--- amr_maldi_ml/scripts/submit_sliding_window_validation_jobs.sh
+++ amr_maldi_ml/scripts/submit_sliding_window_validation_jobs.sh
@@ -6,7 +6,7 @@
 
 # Main command to execute for all combinations created in the script
 # below. The space at the end of the string is important.
-MAIN="poetry run python ../sliding_window_validation.py --duration 8 "
+MAIN="poetry run python ../sliding_window_validation.py --duration 8 --oversampling "
 
 # Try to be smart: if `bsub` does *not* exist on the system, we just
 # pretend that it is an empty command.
@@ -27,7 +27,7 @@ run() {
 for SEED in 344 172 188 270 35 164 545 480 89 409; do
   # Models are ordered by their 'utility' for the project. We are
   # most interested in logistic regression.
-  for MODEL in "lr" "lightgbm"; do
+  for MODEL in "lr" "lightgbm" "mlp"; do
     for SPECIES in "Escherichia coli" "Klebsiella pneumoniae"; do
         CMD="${MAIN} --species \"$SPECIES\" --antibiotic \"Ceftriaxone\" --model $MODEL --seed $SEED"
         run "$CMD";
