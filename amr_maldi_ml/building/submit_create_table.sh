4071aeff83fe56f93d03371949895bb2e8c0a26a
Caroline V Weis <caroline.weis@bsse.ethz.ch>
2021-05-21 14:10:56 +0200

    change to take input files, not path

--- amr_maldi_ml/building/submit_create_table.sh
+++ amr_maldi_ml/building/submit_create_table.sh
@@ -1,4 +1,6 @@
 # ------------
 # Table: Rejection ratio tables
 # ------------
-bsub -W 23:59 -o "rejection_tables_%J.out" -R "rusage[mem=16000]" "poetry run python build_finegrid_assymetric_rejection.py ../../results/calibrated_classifiers/lr"
+bsub -W 47:59 -o "rejection_tables_%J.out" -R "rusage[mem=16000]" "poetry run python build_finegrid_assymetric_rejection.py ../../results/calibrated_classifiers/lr"
+bsub -W 47:59 -o "rejection_tables_%J.out" -R "rusage[mem=16000]" "poetry run python build_finegrid_assymetric_rejection.py ../../results/calibrated_classifiers/lightgbm"
+bsub -W 47:59 -o "rejection_tables_%J.out" -R "rusage[mem=16000]" "poetry run python build_finegrid_assymetric_rejection.py ../../results/calibrated_classifiers/mlp"
