c0e55355bbd54b8de3cd03e8a747ad912ab0a646
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-21 15:20:31 +0200

    undeprecate and update validation_per_species_and_antibiotic plots

--- amr_maldi_ml/plotting/create_plots.sh
+++ amr_maldi_ml/plotting/create_plots.sh
@@ -16,29 +16,45 @@
 # ------------
 # Figure 4: AUC curves per species and antibiotic
 # ------------
+#for model in "lr" "lightgbm" "mlp"; do
+#  python plot_curves_per_species_and_antibiotic.py \
+#    --species "Escherichia coli" \
+#    --antibiotic \
+#      "Ciprofloxacin,Ceftriaxone,Cefepime,Piperacillin-Tazobactam,Tobramycin" \
+#    --model $model \
+#    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Ecoli_$model"
+#done
+#
+#for model in "lr" "lightgbm" "mlp"; do
+#  python plot_curves_per_species_and_antibiotic.py \
+#    --species "Staphylococcus aureus" \
+#    --antibiotic "Ciprofloxacin,Fusidic acid,Oxacillin,Penicillin" \
+#    --model $model \
+#    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Saureus_$model"
+#done
+#
+#for model in "lr" "lightgbm" "mlp"; do
+#  python plot_curves_per_species_and_antibiotic.py \
+#    --species "Klebsiella pneumoniae" \
+#    --antibiotic \
+#      "Ciprofloxacin,Ceftriaxone,Cefepime,Meropenem,Tobramycin" \
+#    --model $model \
+#    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Kpneu_$model"
+#done
 
-for model in "lr" "lightgbm" "mlp"; do
-  python plot_curves_per_species_and_antibiotic.py \
-    --species "Escherichia coli" \
-    --antibiotic \
-      "Ciprofloxacin,Ceftriaxone,Cefepime,Piperacillin-Tazobactam,Tobramycin" \
-    --model $model \
-    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Ecoli_$model"
-done
 
-for model in "lr" "lightgbm" "mlp"; do
-  python plot_curves_per_species_and_antibiotic.py \
-    --species "Staphylococcus aureus" \
-    --antibiotic "Ciprofloxacin,Fusidic acid,Oxacillin,Penicillin" \
-    --model $model \
-    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Saureus_$model"
-done
-
-for model in "lr" "lightgbm" "mlp"; do
-  python plot_curves_per_species_and_antibiotic.py \
-    --species "Klebsiella pneumoniae" \
-    --antibiotic \
-      "Ciprofloxacin,Ceftriaxone,Cefepime,Meropenem,Tobramycin" \
-    --model $model \
-    --outfile "../plots/curves_per_species_and_antibiotic_calibrated/Kpneu_$model"
-done
+# ------------
+# Validation per species and antibiotic 
+# ------------
+poetry run python plot_validation_per_species_and_antibiotic.py --species 'Escherichia coli' \
+                                                                --antibiotic Ceftriaxone \
+                                                                --outfile 'Escherichia_coli_Ceftriaxone_lightgbm' \
+                                                                -m lightgbm
+poetry run python plot_validation_per_species_and_antibiotic.py --species 'Klebsiella pneumoniae' \
+                                                                --antibiotic Ceftriaxone \
+                                                                --outfile 'Klebsiella_pneumoniae_Ceftriaxone_mlp' \
+                                                                -m mlp
+poetry run python plot_validation_per_species_and_antibiotic.py --species 'Staphylococcus aureus' \
+                                                                --antibiotic Oxacillin \
+                                                                --outfile 'Staphylococcus_aureus_Oxacillin_lightgbm' \
+                                                                -m lightgbm
