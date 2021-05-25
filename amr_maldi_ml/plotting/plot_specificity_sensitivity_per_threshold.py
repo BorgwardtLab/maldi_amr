d4f264169899e11669a22791af0f2153ac093cbc
Caroline Weis <caroline.weis@bsse.ethz.ch>
2021-05-25 12:52:30 +0200

    make tablename configurable, add new plots

--- amr_maldi_ml/plotting/plot_specificity_sensitivity_per_threshold.py
+++ amr_maldi_ml/plotting/plot_specificity_sensitivity_per_threshold.py
@@ -1,28 +1,35 @@
 """ Script to plot sensitivity-specificity pairs of rejection threshold. """
 
 import os
+import argparse
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# TODO make configurable
-tablename = '/links/groups/borgwardt/Projects/maldi_tof_diagnostics/amr_maldi_ml/amr_maldi_ml/tables/Rejection_table_assymetric_finegrid_Escherichia_coli_Cefepime_calibrated_lightgbm.csv'
-df = pd.read_csv(tablename)
+if __name__ == '__main__':
 
-# plot sensitivity vs. specificity
-plt.figure(figsize=(10,10))
-sns.set(style="whitegrid")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('INPUT', type=str)
+    args = parser.parse_args()
 
-plt.scatter(x=df['specificity'], 
-            y=df['sensitivity'], 
-            s=4, 
-            cmap='tab10',
-            c=df['percentage rejected samples'],
-            )
-plt.xlabel('specificity (percentage of susceptible samples correctly identified)')
-plt.ylabel('sensitivity (percentage of resistant samples correctly identified)')
+    # read in threshold table
+    df = pd.read_csv(args.INPUT)
 
-plt.colorbar()
+    # plot sensitivity vs. specificity
+    plt.figure(figsize=(10,10))
+    sns.set(style="whitegrid")
 
-filename = os.path.basename(tablename).replace('.csv', '')
-plt.savefig(f'../plots/sensitivity_vs_specificity/{filename}.png')
+    plt.scatter(x=df['specificity'], 
+                y=df['sensitivity'], 
+                s=4, 
+                cmap='tab10',
+                c=df['percentage rejected samples'],
+                )
+    plt.xlabel('specificity (percentage of susceptible samples correctly identified)')
+    plt.ylabel('sensitivity (percentage of resistant samples correctly identified)')
+
+    plt.colorbar()
+
+    filename = os.path.basename(args.INPUT).replace('.csv', '')
+    plt.savefig(f'../plots/sensitivity_vs_specificity/{filename}.png')
